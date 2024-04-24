33dcee5e48f5f6da8c7ef24c0da584d03f131589
ykbeili <ykbeili@gmail.com>
2024-04-23 21:11:19 -0600

    add auto glass params in active admin

--- app/admin/auto_glasses.rb
+++ app/admin/auto_glasses.rb
@@ -1,7 +1,11 @@
 ActiveAdmin.register AutoGlass do
-    permit_params :description
+    permit_params :description, :availability, :body_style, :created_at, :delivery_option, :description, :id, :id_value, :make, :model, :part_number, :price, :quantity, :ships_within_days, :updated_at, :year
+
   
-    filter :description
+    filter :make
+    filter :model
+    filter :year
+    filter :make
     filter :created_at
     filter :updated_at
   
