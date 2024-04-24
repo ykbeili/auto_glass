92073d5f5e83e204e506bbd6916548da7e0b5dce
ykbeili <ykbeili@gmail.com>
2024-04-23 21:02:53 -0600

    add customer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_24_023246) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_24_030239) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -49,6 +49,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_24_023246) do
     t.string "body_style"
     t.float "price"
     t.integer "delivery_option"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
