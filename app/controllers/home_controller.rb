04641ee0df1d7c6c47e4f7c9df38a6c2322797ec
ykbeili <ykbeili@gmail.com>
2024-04-24 20:44:00 -0600

    Add basic search functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
     # before_action :authenticate_customer!
     def index
+      @years = AutoGlass.pluck(:year).compact
+      @models = AutoGlass.pluck(:model).compact
+      @makes = AutoGlass.pluck(:make).compact
+      @auto_glasses = AutoGlass.where(year: params[:year], make: params[:make], model: params[:model])
+      puts(@auto_glasses)
+      puts("auto_glasses")
     end
 end
+  
