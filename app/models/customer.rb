173c5e8731d43302712b58541424339a1151eadc
ykbeili <ykbeili@gmail.com>
2024-04-24 19:43:53 -0600

    Add signin, sign up, logout and profile links

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,7 +4,7 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         def self.ransackable_attributes(auth_object = nil)
-            ["created_at", "email", "encrypted_password", "first_name", "id", "id_value", "last_name", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at"]
-          end
+    def self.ransackable_attributes(auth_object = nil)
+        ["created_at", "email", "encrypted_password", "first_name", "id", "id_value", "last_name", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at"]
+    end
 end
