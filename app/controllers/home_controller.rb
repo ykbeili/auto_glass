8b4e529625532a15e6c88a5d7a5ec72f169eaa3f
ykbeili <ykbeili@gmail.com>
2024-04-28 20:33:57 -0600

    Add devise customers views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,10 @@
 class HomeController < ApplicationController
-    # before_action :authenticate_customer!
+    before_action :authenticate_customer!
     def index
-      @years = AutoGlass.pluck(:year).compact
-      @models = AutoGlass.pluck(:model).compact
-      @makes = AutoGlass.pluck(:make).compact
+      @years = AutoGlass.distinct.order(:year).pluck(:year).compact
+      @models = AutoGlass.distinct.order(:year).pluck(:model).compact
+      @makes = AutoGlass.distinct.order(:make).pluck(:make).compact
       @auto_glasses = AutoGlass.where(year: params[:year], make: params[:make], model: params[:model])
-      puts(@auto_glasses)
-      puts("auto_glasses")
     end
 end
   
