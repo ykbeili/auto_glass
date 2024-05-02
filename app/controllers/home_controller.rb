ba44aca9cca9fe6f20bbb80c53c5891282be2fc5
ykbeili <ykbeili@gmail.com>
2024-05-01 20:25:37 -0600

    add js script to render and hide select menu based on values change

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,11 @@
 class HomeController < ApplicationController
-    before_action :authenticate_customer!
-    def index
-      @years = AutoGlass.distinct.order(:year).pluck(:year).compact
-      @models = AutoGlass.distinct.order(:year).pluck(:model).compact
-      @makes = AutoGlass.distinct.order(:make).pluck(:make).compact
-      @auto_glasses = AutoGlass.where(year: params[:year], make: params[:make], model: params[:model])
-    end
+  before_action :authenticate_customer!
+
+  def index
+    @years = AutoGlass.distinct.order(:year).pluck(:year).compact
+    @models = AutoGlass.distinct.order(:year).pluck(:model).compact
+    @makes = AutoGlass.distinct.order(:make).pluck(:make).compact
+    @auto_glasses = AutoGlass.where(year: params[:year], make: params[:make], model: params[:model])
+  end
 end
   
