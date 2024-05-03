3e5f8a4f5f35aad3014b0aefcc1e4259fa15d9dd
ykbeili <ykbeili@gmail.com>
2024-05-02 20:46:06 -0600

    add relationships between auto_glass customer and order

--- app/models/auto_glass.rb
+++ app/models/auto_glass.rb
@@ -1,5 +1,7 @@
 class AutoGlass < ApplicationRecord
-    def self.ransackable_attributes(auth_object = nil)
-        ["availability", "body_style", "created_at", "delivery_option", "description", "id", "id_value", "make", "model", "part_number", "price", "quantity", "ships_within_days", "updated_at", "year"]
-    end
+  has_many :order_items
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["availability", "body_style", "created_at", "delivery_option", "description", "id", "id_value", "make", "model", "part_number", "price", "quantity", "ships_within_days", "updated_at", "year"]
+  end
 end
