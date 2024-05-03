bc25b504571c382bedbd040331491d1fe1925587
ykbeili <ykbeili@gmail.com>
2024-05-02 21:18:05 -0600

    add order show method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,3 +1,20 @@
 class OrdersController < ApplicationController
+  before_action :find_order, only: [:show]
+
+  def create
+    @auto_glasses = AutoGlass.where(id: params[:auto_glass_ids])
+    @order = Order.new()
+    @order.customer = current_customer
+    @order.save!
+    redirect_to order_path(@order)
+  end
+
+  def show
+  end
+
+  private
   
+  def find_order
+    @order = Order.find(params[:id])
+  end
 end
