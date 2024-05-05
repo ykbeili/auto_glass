145c6f19a160a9f479af8eea7facd3234ca63bbc
ykbeili <ykbeili@gmail.com>
2024-05-04 21:43:23 -0600

    Add orders to active admin panel

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,10 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items
 
+  def self.ransackable_associations(auth_object = nil)
+    ["customer", "order_items"]
+  end
+
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "customer_id", "id", "id_value", "quantity", "total_price", "updated_at"]
   end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,8 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :auto_glass
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["auto_glass_id", "created_at", "id", "id_value", "order_id", "price", "quantity", "updated_at"]
+  end
 end
